fc7c456eca3662e59ec5ac176772e23c4e817057
Shajibul Hasan Soaib <shajibulhasan.puc@gmail.com>
2025-07-07 16:42:39 +0600

    syntex file added

---
+++ bin/syntex.dart
@@ -0,0 +1,10 @@
+void main (){
+  var name = "Soaib";//any data type
+  print("hello $name");
+
+  final String language = "dart"; //compile time & run time not change able
+ // final key word initialized at run time
+
+  const num = 66;
+  // const key word initialized at compile time
+}
