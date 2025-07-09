015e764fb31e2f15feb38c84a207cb7db9e0b514
shajibulhasan <shajibulhasan.puc@gmail.com>
2025-07-09 22:39:10 +0600

    map

---
+++ bin/map.dart
@@ -0,0 +1,11 @@
+void main(){
+  Map<String, dynamic> students = {
+    'name' : 'shajibul',
+    'age' : '20',
+    'phone' : '01571171044'
+  };
+
+  print(students);
+
+  print("Student Name: ${students['name']}");
+}
